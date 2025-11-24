ec0a00367595bc5a67ed2d76f7b292a0d3cad71f
yousefa7med <youssefahmedserag@gmail.com>
2025-11-24 23:50:54 +0200

    add app icon and assets file

--- lib/features/auth/presentation/views/register_view.dart
+++ lib/features/auth/presentation/views/register_view.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-class LoginView extends StatelessWidget {
-  const LoginView({super.key});
+class RegisterView extends StatelessWidget {
+  const RegisterView({super.key});
 
   @override
   Widget build(BuildContext context) {
